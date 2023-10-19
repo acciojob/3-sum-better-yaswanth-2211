450ae6b32d380dac830bcaaf27d7d4f5828ed488
yaswanth-2211 <145182914+yaswanth-2211@users.noreply.github.com>
2023-10-19 22:51:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for(i=0;i<arr.length-3;i++){
     for(j=i+1;j<arr.length-2;j++){
 		for(k=j+1;k<arr.length-1;k++){
 				let sum=arr[i]+arr[j]+arr[k];
-				let c=Math.abs(sum-target);
+				let diff=Math.abs(sum-target);
 				if(initialdiff>diff){
 					initialdiff=diff
 					ans=sum;
